7450c28e089b5730cb1df82a7e24ccbf3e56ef37
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-11-13 19:31:18 -0300

    feat: update formdataParser

--- middleware-core/src/main/java/ufrn/server/http/util/FormDataParser.java
+++ middleware-core/src/main/java/ufrn/server/http/util/FormDataParser.java
@@ -9,28 +9,44 @@ public class FormDataParser {
 
     public static Map<String, String> parseFormData(BufferedReader reader) throws IOException {
         Map<String, String> formData = new HashMap<>();
-        var stop = false;
-        String line;
-        while ((line = reader.readLine()) != null || stop) {
+        // Read the content type line
+        String contentTypeLine = reader.readLine();
 
-            if (line.startsWith("Content-Disposition: form-data")) {
-                // Processar a linha de Content-Disposition
-                String[] dispositionParts = line.split("; ");
-                for (String part : dispositionParts) {
-                    if (part.startsWith("name=")) {
-                        String paramName = part.substring("name=".length() + 1, part.length() - 1);
-                        if (paramName.equals("file")) {
-                            stop = true;
-                        }
-                        line = reader.readLine();
-                        String paramValue = line;
-                        while (!line.startsWith("-")) {
-                            paramValue = paramValue.concat("\n").concat(line);
-                            line = reader.readLine();
-                        }
-                        formData.put(paramName, paramValue);
+        // Read the boundary from the content type line
+        String boundary = "--------" + contentTypeLine.substring(contentTypeLine.indexOf("boundary=") + 9);
+
+        // Process each part in the multipart content
+        String line = boundary;
+        while (line != null) {
+            if (line.equals(boundary)) {
+                // Read headers for the part
+//                line = reader.readLine();
+                Map<String, String> headers = new HashMap<>();
+                while (line != null && !line.trim().isEmpty()) {
+                    String[] headerParts = line.split(": ");
+                    if (headerParts.length == 2) {
+                        headers.put(headerParts[0].trim(), headerParts[1].trim());
                     }
+                    line = reader.readLine();
+                    System.out.println("i>> " + line);
                 }
+//                return formData;
+//              Read the part content
+                StringBuilder content = new StringBuilder();
+                line = reader.readLine();
+                while (line != null && !line.contains(boundary)) {
+                    content.append(line);
+                    content.append("\r\n");
+                    line = reader.readLine();
+                }
+                // Extract the part name and value
+                String partContent = content.toString().trim();
+                String partName = headers.get("Content-Disposition").split(";")[1].trim().split("=")[1].replaceAll("\"", "");
+                formData.put(partName, partContent);
+//                return formData;
+            } else {
+                System.out.println("e>> " + line);
+                break;
             }
         }
 
